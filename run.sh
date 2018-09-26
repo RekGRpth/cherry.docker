26637cc1c41435af92f6413ab5d4b92e98c2657b
RekGRpth <rekgrpth@gmail.com>
2018-09-26 08:48:23 +0500

    up

--- run.sh
+++ run.sh
@@ -8,19 +8,17 @@ docker pull rekgrpth/cherry || exit $?
 docker volume create cherry || exit $?
 docker network create my
 docker run \
-    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host cherry-`hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
-    --add-host ldap.t72.ru:`getent hosts ldap.t72.ru | cut -d ' ' -f 1` \
+    --add-host ldap.t72.ru:$(getent hosts ldap.t72.ru | cut -d ' ' -f 1) \
     --detach \
     --env USER_ID=$(id -u) \
     --env GROUP_ID=$(id -g) \
-    --env HOSTNAME=cherry-`hostname -f` \
+    --env HOSTNAME=cherry-$(hostname -f) \
     --hostname cherry \
     --name cherry \
     --network my \
     --restart always \
-    --volume /etc/certs/`hostname -d`.crt:/etc/ssl/apache2/server.pem \
-    --volume /etc/certs/`hostname -d`.key:/etc/ssl/apache2/server.key \
+    --volume /etc/certs/$(hostname -d).crt:/etc/ssl/apache2/server.pem \
+    --volume /etc/certs/$(hostname -d).key:/etc/ssl/apache2/server.key \
     --volume cherry:/data \
     --volume /var/lib/docker/volumes/cherry/_data/apache2.conf:/etc/apache2/conf.d/ssl.conf \
     rekgrpth/cherry

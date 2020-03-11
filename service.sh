4a7f687647fe907468dab550f776201124ef52b9
RekGRpth <rekgrpth@gmail.com>
2020-03-11 10:46:07 +0500

    up

--- service.sh
+++ service.sh
@@ -4,19 +4,15 @@
 #docker push rekgrpth/cherry || exit $?
 docker pull rekgrpth/cherry || exit $?
 docker volume create cherry || exit $?
-docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
-docker stop cherry || echo $?
-docker rm cherry || echo $?
-docker run \
-    --detach \
+docker network create --attachable --driver overlay docker || echo $?
+docker service create \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
     --hostname cherry \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs \
+    --mount type=volume,source=cherry,destination=/home \
     --name cherry \
     --network name=docker \
-    --restart always \
-    --volume /etc/certs:/etc/certs \
-    --volume cherry:/home \
     rekgrpth/cherry uwsgi --ini cherry.ini

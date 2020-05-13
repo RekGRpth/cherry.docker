b09d4f2ffd32fb2e0aec4ad5806641bd74db850a
RekGRpth <rekgrpth@gmail.com>
2020-05-13 15:56:28 +0500

    up

--- service.sh
+++ service.sh
@@ -1,16 +1,18 @@
 #!/bin/sh -ex
 
-#docker build --tag rekgrpth/cherry . || exit $?
-#docker push rekgrpth/cherry || exit $?
-docker pull rekgrpth/cherry || exit $?
-docker volume create cherry || exit $?
+#docker build --tag rekgrpth/cherry .
+#docker push rekgrpth/cherry
+docker pull rekgrpth/cherry
+docker volume create cherry
 docker network create --attachable --driver overlay docker || echo $?
+docker service rm cherry || echo $?
 docker service create \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
-    --hostname cherry \
+    --hostname="{{.Service.Name}}-{{.Node.Hostname}}" \
+    --mode global \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
     --mount type=bind,source=/run/uwsgi,destination=/run/uwsgi \

a72b31d035379717355bae9f64d0c25e9abd5fb5
RekGRpth <rekgrpth@gmail.com>
2021-10-17 09:43:48 +0500

    edge

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag rekgrpth/cherry . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/cherry.docker . 2>&1 | tee build.log
